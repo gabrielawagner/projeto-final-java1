3d974411790719e5e02236c6cb8d0db4b18ff558
João Barcelos Coutinho <jabcneto@gmail.com>
2020-08-08 19:10:06 -0300

    tentando fazer o menu

--- src/br/com/serratec/javaFinal/menus/Menu.java
+++ src/br/com/serratec/javaFinal/menus/Menu.java
@@ -1,8 +1,10 @@
 package br.com.serratec.javaFinal.menus;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 import br.com.serratec.javaFinal.contaBancaria.ContaCorrente;
+import br.com.serratec.javaFinal.contaBancaria.ContaPoupanca;
 import br.com.serratec.javaFinal.contaBancaria.Tributos;
 import br.com.serratec.javaFinal.pessoal.Funcionario;
 
@@ -10,18 +12,22 @@ public class Menu {
 	Scanner input = new Scanner(System.in);
 	ContaCorrente cc = new ContaCorrente(4, "123.456.789-10", 2000.0);
 	ContaCorrente cc2 = new ContaCorrente(1, "123.456.789-10", 2000.0);
-	//TODO Perguntar ao marcelo como resolver a navegação do menu bug ao gerar relatorio.
-	
+	ContaPoupanca cp = new ContaPoupanca(1, "123.456.789-10", 2000.0);
+	// TODO Perguntar ao marcelo como resolver a navegação do menu bug ao gerar
+	// relatorio.
+
 	public void cliente(Funcionario f) {
+		boolean sair = false;
+
 		System.out.println("Bem vindo escolha: [1]Movimentações ou [2]Relatórios");
 		int resposta = input.nextInt();
 		switch (resposta) {
 		case 1:
-			
 			movimentacoesConta(f);
 			break;
 		case 2:
-			System.out.println("Saldo [1] ou [2] Tributação da conta corrente");
+			System.out.println(
+					"[1]-Saldo [2]-Tributação da conta corrente [3]-Simulador Poupança [4]-Voltar [5]-Finalizar ");
 			int resposta2 = input.nextInt();
 			switch (resposta2) {
 			case 1:
@@ -29,15 +35,32 @@ public class Menu {
 				System.out.println(cc.getSaldo());
 				break;
 			case 2:
-				System.out.println("Tributação total da conta: ");
+				System.out.println("Tributação Total da Conta Corrente: ");
 				double tributoDeposito = (cc.getQuantidadeDeposito() * Tributos.depositoTributo);
 				double tributoSaque = (cc.getQuantidadeSaque() * Tributos.saqueTributo);
 				double tributoTransferencia = (cc.getQuantidadeTranferencia() * Tributos.transferenciaTributo);
 				System.out.println(tributoDeposito + tributoSaque + tributoTransferencia);
+				break;
+			case 3:
+				// TODO Metodo de calcular apartir da data inserida
+				System.out.println("Simulador de Rendimento da Poupança: ");
+				System.out.print("Valor: ");
+				double valor = input.nextDouble();
+				System.out.print("Quantidade de dias: ");
+				int dias = input.nextInt();
+				double rendimentoDiario = cp.getRendimento() / 30;
+				System.out.println("Seu rendimento foi: " + (valor * (dias * rendimentoDiario) / 100));
+				break;
+			case 4:
+				cliente(f);
+				break;
+			case 5:
+				sair = true;
+				break;
 			}
 		}
 	}
-	
+
 	public void principal(Funcionario f) {
 		String tipo = f.getCargo();
 		switch (tipo) {
@@ -60,7 +83,6 @@ public class Menu {
 				System.out.println("--------------------------------------------");
 				double valorSaque = input.nextDouble();
 				cc.sacar(valorSaque);
-				resposta = 0;
 				break;
 			case 2:
 				System.out.println("\n------------------Deposito-------------------");
@@ -77,13 +99,10 @@ public class Menu {
 				cc.transfere(cc2, valorTransferencia);
 				break;
 			case 4:
-				principal(f); 
+				principal(f);
 				break;
 			case 5:
 				sair = true;
-				break;
-			default:
-				System.out.println("Operação Invalida!");
 				break;
 			}
 		} while (sair == false);
